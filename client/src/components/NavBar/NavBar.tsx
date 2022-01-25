c55b7f8538602994d5c82e34d320b2bdd4d4ae59
arthurmtro <arthurmtro@gmail.com>
2022-01-25 15:39:45 +0100

    adding giga ultra mega alpha logo

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -4,8 +4,12 @@ import styles from "./NavBar.module.css";
 export default function NavBar() {
   return (
     <section className={styles["status-bar"]}>
-      <h1>LOGO</h1>
       <ul>
+        <img
+          style={{ width: "15rem", marginBottom: "4rem" }}
+          alt="404"
+          src="https://i.pinimg.com/originals/ce/57/77/ce5777bf7b7bd966ef14f4a0c7e4c845.gif"
+        />
         <NavLink path="/" label="Accueil" icon="home" />
         <NavLink path="/running" label="Running" icon="running" />
         <NavLink
