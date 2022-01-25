6dad86fa04037f1bd9537f672fe7b48ca1e86d30
arthurmtro <arthurmtro@gmail.com>
2022-01-25 15:35:19 +0100

    correction, adding flex design to progress bar

--- client/src/components/ProgressBar/ProgressBar.tsx
+++ client/src/components/ProgressBar/ProgressBar.tsx
@@ -3,8 +3,7 @@ import styles from "./ProgressBar.module.css";
 export default function Box() {
   return (
     <div className={styles.background}>
-      <div>A</div>
-      <progress id="file" max="100" value="30" className={styles.progress}> 30% </progress>
+      <progress id="file" max="100" value="30" className={styles.progress} />
     </div>
   );
 }
