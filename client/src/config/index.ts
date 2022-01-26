9cdc2682da345e577e9251440ceabc51304f8906
arthurmtro <arthurmtro@gmail.com>
2022-01-26 10:19:02 +0100

    add env variables

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,7 +1,4 @@
 console.log("process.env :>> ", process.env);
 
-export const base_url =
-  process.env.NODE_ENV === "development"
-    ? `http://localhost:8080`
-    : `http://${process.env.API_URL}:8080`;
+export const base_url = `http://${process.env.REACT_APP_API_URL}:8080`;
 export const api_url = `${base_url}/api`;
