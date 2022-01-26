0b029848a2b1ddf5d62acd27c92732931f728b0c
arthurmtro <arthurmtro@gmail.com>
2022-01-26 17:19:49 +0100

    init timer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Routes, Route } from "react-router-dom";
 
 // Contexts
 import CylindersDataProvider from "./contexts/cylindersProdiver";
 import ProfilesDataProvider from "./contexts/profilesProvider";
-import RunningProfileProvider, {
-  useRunningProfile,
-} from "./contexts/runningProvider";
+import RunningProfileProvider from "./contexts/runningProvider";
 
 // Components
 import Layout from "./components/Layout";
@@ -15,45 +12,7 @@ import HomePage from "./pages/Home";
 
 const queryClient = new QueryClient();
 
-var interval: NodeJS.Timer;
-
 export default function App() {
-  const [startedTime, setStartedTime] = useState(0);
-
-  const { runningProfile, setRunningProfile, timeSpend, setTimeSpend } =
-    useRunningProfile();
-
-  useEffect(() => {
-    if (runningProfile === null) {
-      return setTimeSpend(0);
-    }
-
-    const d = new Date();
-    setStartedTime(Math.round(d.getTime()));
-
-    console.log("StartedTime :>> ", Math.round(d.getTime()));
-
-    interval = setInterval(() => {
-      const d = new Date();
-      setTimeSpend(Math.round(d.getTime()) - startedTime);
-
-      console.log("timeSpend :>> ", Math.round(d.getTime()) - startedTime);
-      console.log("runningProfile?.duration :>> ", runningProfile.duration);
-    }, 1000);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [runningProfile]);
-
-  useEffect(() => {
-    if (timeSpend === 0) return;
-    if (runningProfile === null || timeSpend >= runningProfile?.duration) {
-      clearInterval(interval);
-      setTimeSpend(0);
-      setRunningProfile(null);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [timeSpend]);
-
   return (
     <QueryClientProvider client={queryClient}>
       <CylindersDataProvider>
--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,5 +1,10 @@
 import styles from "./Layout.module.css";
+import { useEffect, useState } from "react";
 
+// Context
+import { useRunningProfile } from "../../contexts/runningProvider";
+
+// Components
 import NavBar from "../NavBar";
 import StatusBar from "../StatusBar";
 
@@ -7,7 +12,45 @@ type ParamsType = {
   children: React.ReactNode;
 };
 
+var interval: NodeJS.Timer;
+
 export default function Layout({ children }: ParamsType) {
+  const [startedTime, setStartedTime] = useState(0);
+
+  const { runningProfile, setRunningProfile, timeSpend, setTimeSpend } =
+    useRunningProfile();
+
+  useEffect(() => {
+    if (runningProfile === null) {
+      return setTimeSpend(0);
+    }
+
+    const d = new Date();
+    setStartedTime(Math.round(d.getTime()));
+
+    console.log("StartedTime :>> ", Math.round(d.getTime()));
+
+    interval = setInterval(() => {
+      const d = new Date();
+      setTimeSpend(Math.round(d.getTime()) - startedTime);
+
+      console.log("timeSpend :>> ", Math.round(d.getTime()) - startedTime);
+      console.log("runningProfile?.duration :>> ", runningProfile.duration);
+    }, 1000);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [runningProfile]);
+
+  useEffect(() => {
+    if (timeSpend === 0) return;
+    if (runningProfile === null || timeSpend >= runningProfile?.duration) {
+      clearInterval(interval);
+      setTimeSpend(0);
+      setRunningProfile(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timeSpend]);
+
   return (
     <div className={styles.layout}>
       <NavBar />
