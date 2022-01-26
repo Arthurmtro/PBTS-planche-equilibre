05f11f7898d0d5c87ba2f16c9c64e5f59eb01ff7
arthurmtro <arthurmtro@gmail.com>
2022-01-26 19:00:34 +0100

    add api url

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -18,7 +18,7 @@ export default function NavBar() {
           icon="configuration"
         />
       </ul>
-      <span>Sheeshh</span>
+      <span>{process.env.REACT_APP_API_URL}</span>
     </section>
   );
 }
