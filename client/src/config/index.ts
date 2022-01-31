9dc516ce8ae8d03153296702c845ce621fc88c2c
MatthieuQuerel <querelmatthieu@gmail.com>
2022-01-31 12:05:57 +0100

    initialiser

--- client/src/config/index.ts
+++
@@ -1,2 +0,0 @@
-export const base_url = `http://${process.env.REACT_APP_API_URL}:8080`;
-export const api_url = `${base_url}/api`;
---
+++ index.js
@@ -0,0 +1,50 @@
+////////////////////////////////////////bibliothèque///////////////////////////////////////////////////////
+var i2cBus = require("i2c-bus"); // bibliothèque pour les pine
+var Pca9685Driver = require("pca9685").Pca9685Driver; // bibliothèque pour PCA9685
+
+/////////////////////////////////////////option/////////////////////////////////////////////////////////////
+
+var options = {
+  // option de la carte bleu
+  i2c: i2cBus.openSync(1), //  ouvre le port i2c
+  address: 0x40, // adresse carte
+  frequency: 50,
+  debug: true,
+};
+
+////////////////////////////////////////option/////////////////////////////////////////////////////////////////////
+
+const pwm = new Pca9685Driver(options, function (err) {
+  // gère les erreur sa revoie si
+  //   il y a une ereure sur la carte avec une alocation dynamique
+  if (err) {
+    // si il y a un probleme avec la carte quand on log il nous renvoie une erreur
+    console.error("Erreur d'initialisation PCA9685");
+    process.exit(-1);
+  }
+
+  console.log("Initialisation effectuée");
+
+  ////////////////////////////////////fonction////////////////////////////////////////////
+  // le circuit electrique si on fais tourner plusier pro en meme temps il coup instantanément 1 pour faire marcher les autre
+
+  pwm.allChannelsOff();
+
+  // il fais avancer et reculer mais on choisie la vitesse sa vas de 0.1 a 1
+  pwm.setDutyCycle(4, 1); // gérer 1 verin 5 desendre / 4 monter
+  pwm.setDutyCycle(2, 1); // gérer l autre 3 desendre / 2 monter
+  pwm.setDutyCycle(7, 1); // gérer l autre 7 desendre / 6 monter
+
+  /* pwm.setPulseRange(1, 50, 255, function() {
+         if (err) {
+             console.error("Error setting pulse range.");
+         } else {
+             console.log("Pulse range set.");
+            }
+ */
+
+  //pwm.setPulseLength(0, 300);// pour le serveau moteur sa marche
+  // a 100% le chanel il fais avancer et reculer le verrin
+  //pwm.channelOff(5);
+  //pwm.channelOn(4);
+});
