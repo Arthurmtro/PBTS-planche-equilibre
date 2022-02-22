9133553b53c932bc318a5e3486a86d1c630fad8d
arthurmtro <arthurmtro@gmail.com>
2022-02-22 10:57:27 +0100

    removing useless view uses

--- api/src/app.ts
+++ api/src/app.ts
@@ -12,10 +12,6 @@ import routes from "./routes"
 
 const app = express()
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "jade")
-
 app.use(cors())
 app.use(logger("dev"))
 app.use(express.json())
@@ -36,8 +32,7 @@ app.use((err, req, res, _next) => {
 	res.locals.error = req.app.get("env") === "development" ? err : {}
 
 	// render the error page
-	res.status(err.status || 500)
-	res.render("error")
+	res.sendStatus(err.status || 500)
 })
 
 export default app
