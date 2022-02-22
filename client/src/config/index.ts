cd7af65dd8d68e9a8cc421d04fb8308e076be3ee
arthurmtro <arthurmtro@gmail.com>
2022-02-22 10:59:02 +0100

    api route

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,2 +1,1 @@
-export const base_url = `http://${process.env.REACT_APP_API_URL}:8080`;
-export const api_url = `${base_url}/api`;
+export const api_url = `http://${process.env.REACT_APP_API_URL}:8080`
