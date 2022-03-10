0bf8fec5bbdc1715997c79709d1660e33cb2f329
MathisLandreau <landreau.mathis23@gmail.com>
2022-03-10 12:09:09 +0100

    page edit profile

--- client/src/components/Slider/Slider.tsx
+++ client/src/components/Slider/Slider.tsx
@@ -1,13 +1,25 @@
-import { CSSProperties } from "react";
+import { useState } from "react";
 import styles from "./Slider.module.css";
 
+type SliderType={
+  label?: string
+  nbverin: Number
+  min?: number
+  max?: number
+}
 
+export default function Slider({label,min=0,max=100}:SliderType) {
+const [progressVal, setProgressVal] = useState<number>(0)
 
-export default function Slider() {
   return (
-  <div>
-    <label>Pourcentage</label>
-    <input type="range" id="Pourcentage" name="Pourcentage"min="0" max="100"/>
+  <div className={styles.slider}>
+    <label>{label}</label>
+    <input type="range" id={label} name={label} min={min} max={max} step="1" value={progressVal} onChange={(event) => {
+      setProgressVal(Number(event.target.value))
+    }}/>
+    <input type="number" name="" id="" min={min} max={max} value={progressVal} onChange={(event) => {
+      setProgressVal(Number(event.target.value))
+    }}/>
 
   </div>
   );
--- client/src/pages/EditProfile.tsx
+++ client/src/pages/EditProfile.tsx
@@ -1,4 +1,4 @@
-//import styles from "../styles/Running.module.css";
+import styles from "../styles/EditProfile.module.css";
 
 import Slider from "../components/Slider";
 
@@ -6,11 +6,30 @@ import Slider from "../components/Slider";
 
 export default function EditProfilePage() {
 	return (
-		<div>
-			<Slider/>
+		<>
+		<div className={styles["header"]}>
 
+			<div className={styles["right"]} >
+				<output>Verin 1</output>  
+				<Slider label="Pourcentage deploiment" nbverin={1} min={0} max={100}/> 
+				<Slider label="Pourcentage de la vites" nbverin={1} min={0} max={99}/>
+		
+			</div>
+			<div className={styles["centre"]}>
+				<output>Verin 2</output>
+				<Slider nbverin={2}min={0} max={100}/> 
+				<Slider nbverin={2} min={0} max={99}/>
+			</div>
 
-			
+			<div className={styles["left"]}>
+				<output>Verin 3</output>
+				<Slider nbverin={3} min={0} max={100}/> 
+				<Slider nbverin={3} min={0} max={99}/>
+
+			</div>
 		</div>
-	)
-}
+	
+		
+		
+		</>
+	)}
