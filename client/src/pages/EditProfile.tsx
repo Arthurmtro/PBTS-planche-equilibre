03da13470d34e6239acbc67a2ff06cbb21d4eb48
MathisLandreau <landreau.mathis23@gmail.com>
2022-03-14 12:17:04 +0100

    page add profile dinamique

--- client/src/pages/EditProfile.tsx
+++ client/src/pages/EditProfile.tsx
@@ -1,35 +1,52 @@
 import styles from "../styles/EditProfile.module.css";
+import { useState } from "react";
 
 import Slider from "../components/Slider";
 
 
+type CommandsType = {
+	openner: number
+	speed: number
+}
 
-export default function EditProfilePage() {
+
+const OptionList = ({idVerrin, commands} : {idVerrin: number, commands: CommandsType[] }) => {
+	console.log("options = ", idVerrin)
+
 	return (
 		<>
-		<div className={styles["header"]}>
-
-			<div className={styles["right"]} >
-				<output>Verin 1</output>  
-				<Slider label="Pourcentage deploiment" nbverin={1} min={0} max={100}/> 
-				<Slider label="Pourcentage de la vites" nbverin={1} min={0} max={99}/>
-		
+			<p>Verin {idVerrin}</p>  
+			<div className={styles.commands}>
+			{
+				commands.map(() => (
+					<>
+						<Slider label="Pourcentage deploiment" nbverin={idVerrin} min={0} max={100} /> 
+						<Slider label="Pourcentage de la vites" nbverin={idVerrin} min={0} max={99}/>
+					</>
+				))
+			}
 			</div>
-			<div className={styles["centre"]}>
-				<output>Verin 2</output>
-				<Slider nbverin={2}min={0} max={100}/> 
-				<Slider nbverin={2} min={0} max={99}/>
-			</div>
+		</>
+	)
+}
 
-			<div className={styles["left"]}>
-				<output>Verin 3</output>
-				<Slider nbverin={3} min={0} max={100}/> 
-				<Slider nbverin={3} min={0} max={99}/>
+export default function EditProfilePage() {
+	const [verrinIds, setVerrinIds] = useState<number[]>([0,  1, 2])
+	const [commands, setCommands] = useState<CommandsType[]>([])
 
+	// Memo
+	const handleClick = () => {
+		setCommands(prev => [...prev, {
+			openner: 0,
+			speed: 0
+		}])
+	}
+
+	return (
+		<>
+			<div className={styles.verrins}>
+				{verrinIds.map((verrinId =>  <OptionList key={verrinId} idVerrin={verrinId}  commands={commands}  /> ))}
 			</div>
-		</div>
-	
-		
-		
+			<button onClick={() => handleClick()}>Add Line</button>
 		</>
 	)}
