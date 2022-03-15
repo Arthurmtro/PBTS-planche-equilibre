7c6ae4af6b3dfaf0ff6146af2bbc0662bde6c094
arthurmtro <arthurmtro@gmail.com>
2022-03-15 16:41:58 +0100

    create profile and secure route

--- client/src/api/fetchAllProfiles.ts
+++ client/src/api/fetchAllProfiles.ts
@@ -1,11 +1,11 @@
-import { api_url } from "../config";
+import { api_url } from "../config"
 
 export default async function fetchAllProfiles() {
-  const response = await fetch(`${api_url}/fetch-profiles`);
-  if (!response.ok) {
-    throw new Error("Problem fetching profiles infos");
-  }
+	const response = await fetch(`${api_url}/fetch-profiles`)
+	if (!response.ok) {
+		throw new Error("Problem fetching profiles infos")
+	}
 
-  const profiles = await response.json();
-  return profiles;
+	const profiles = await response.json()
+	return profiles
 }
