be9dfb77f1689976020b10130c02e81f7a915288
MatthieuQuerel <querelmatthieu@gmail.com>
2022-03-29 10:54:47 +0200

    jhq

--- tests.js
+++ tests.js
@@ -3,12 +3,13 @@ var assert = require("assert")
 describe("Test Suite 1", function () {
 	it("Test 1", function () {
 		// metre sa fonction
-		setInterval()
-		assert.ok(setInterval(), "Cela ne devrait pas échouer")
-		assert.ok(setInterval(), "Cela ne devrait échouer")
+		readBytes(adrs, length, callback)
+		assert.ok(readBytes(adrs, length, callback), "Cela ne devrait pas échouer")
+		return true
 	})
 
 	it("Test 2", function () {
+		readBytes(adrs, length, callback)
 		assert.ok(1 === 1, "Cela ne devrait pas échouer")
 		assert.ok(false, "Cela devrait échouer")
 	})
