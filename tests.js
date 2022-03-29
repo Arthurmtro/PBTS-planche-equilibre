c960e54297df165318eaafc05b83fb938d8970bf
MatthieuQuerel <querelmatthieu@gmail.com>
2022-03-29 14:10:46 +0200

    sdf

--- tests.js
+++ tests.js
@@ -1,16 +1,19 @@
-var assert = require("assert")
+var i2c = require("./dist/i2c-service")
+var assert = require("assert").strict
+var adrs = 0
+var length = 0
 
 describe("Test Suite 1", function () {
 	it("Test 1", function () {
 		// metre sa fonction
-		readBytes(adrs, length, callback)
-		assert.ok(readBytes(adrs, length, callback), "Cela ne devrait pas échouer")
+		let readBytes = new ReadBit()
+		assert.ok(readBytes.readBytes(adrs, length).length, 1, "Cela ne devrait pas échouer")
 		return true
 	})
 
-	it("Test 2", function () {
+	/*it("Test 2", function () {
 		readBytes(adrs, length, callback)
 		assert.ok(1 === 1, "Cela ne devrait pas échouer")
 		assert.ok(false, "Cela devrait échouer")
-	})
+	})*/
 })
