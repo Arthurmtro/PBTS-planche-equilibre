e543e5f108360acc49869dd062390981872807ac
MatthieuQuerel <querelmatthieu@gmail.com>
2022-04-05 14:32:56 +0200

    kisr

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 ////////////////////////////////////////bibliothèque///////////////////////////////////////////////////////
-var i2cBus = require("i2c-bus"); // bibliothèque pour l i2c ou on inport
-var Pca9685Driver = require("pca9685").Pca9685Driver; // bibliothèque pour PCA9685 ou on import
+
+var i2cBus = require("i2c-bus"); // bibliothèque pour la liaison  hard et saufte
+var Pca9685Driver = require("pca9685").Pca9685Driver; // bibliothèque pour PCA9685
 
 /////////////////////////////////////////option/////////////////////////////////////////////////////////////
 
 var options = {
-  // option de la carte PCA9685
+  // option de la carte bleu
   i2c: i2cBus.openSync(1), //  ouvre le port i2c
   address: 0x40, // adresse carte
   frequency: 50,
   debug: true,
 };
-
 ////////////////////////////////////////option/////////////////////////////////////////////////////////////////////
-
 const pwm = new Pca9685Driver(options, function (err) {
   // gère les erreur sa revoie si
   //   il y a une ereure sur la carte avec une alocation dynamique
@@ -22,22 +21,32 @@ const pwm = new Pca9685Driver(options, function (err) {
     console.error("Erreur d'initialisation PCA9685");
     process.exit(-1);
   }
-
   console.log("Initialisation effectuée");
 
   ////////////////////////////////////fonction////////////////////////////////////////////
-  // le circuit electrique si on fais tourner plusier pro en meme temps il coup instantanément 1 pour faire marcher les autre
 
-  // il fais avancer et reculer mais on choisie la vitesse sa vas de 0.1 a 1 (regler le cicle de service )
-  pwm.setDutyCycle(5, 1); // gérer 1 verin 5 desendre / 4 monter
-  pwm.setDutyCycle(2, 1); // gérer l autre 3 desendre / 2 monter
-  pwm.setDutyCycle(6, 1); // gérer l autre 7 desendre / 6 monter
-  pwm.setDutyCycle(15, 1); // teste osciloscope
-  //pwm.allChannelsOff();// tout eteindre
+  // il fais avancer et reculer mais on choisie la vitesse sa vas de 0.1 a 1
 
-  //pwm.setPulseLength(0, 300); // pour le serveau moteur sa marche (regler sicle d inpultion)
+  pwm.setDutyCycle(3, 1); // gérer 1 verin 7 desendre / 8 monter
+  //pwm.setDutyCycle(4, 1); // gérer l autre 5 desendre / 6 monter
+  //pwm.setDutyCycle(4, 1); // gérer l autre 5 desendre / 6 monter
+
+  /* pwm.setPulseRange(1, 50, 255, function() {
+         if (err) {
+             console.error("Error setting pulse range.");
+         } else {
+             console.log("Pulse range set.");
+            }
+ */
+  //pwm.setPulseLength(0, 300);// pour le serveau moteur sa marche
 
   // a 100% le chanel il fais avancer et reculer le verrin
-  //pwm.channelOff(5);
-  //pwm.channelOn(4);
+  //pwm.channelOff(7);
+  //pwm.channelOn(8);
+
+  // a voir
+  //pwm.setPWM(pin, 0, 4096); //allumer
+  //pwm.setPWM(pin, 4096, 0);// eteindre
+  /////////////////////////////////////////////////////////profile inclinaisonss//////////////////////////////////////////////////
 });
+//pwm.allChannelsOff(); // tout eteindre
